6dbba19ec8966d210aa3f00b720dd98cf2739324
totat com <totat92207@synsky.com>
2026-07-21 17:33:53 +0000

    applied business logics in intermediate_r layer

--- models/intermediate_r/int_sales_r.sql
+++ models/intermediate_r/int_sales_r.sql
@@ -1,13 +1,40 @@
-SELECT
+
+with sales_calc as 
+(SELECT
 o.ORDER_ID,
-o.ORDER_DATE,
-c.CUSTOMER_ID,
+--o.ORDER_DATE,
+--c.CUSTOMER_ID,
 c.CUSTOMER_NAME,
-c.CITY,
-c.STATE
-
+p.PRODUCT_NAME,
+p.CATEGORY,
+oi.QUANTITY,
+oi.UNIT_PRICE,
+oi.QUANTITY * oi.UNIT_PRICE AS GROSS_AMOUNT,
+(oi.QUANTITY * oi.UNIT_PRICE) - oi.DISCOUNT AS NET_SALES,
+oi.quantity * p.cost as tot_cost,
+((oi.quantity * oi.unit_price) - oi.discount) - (
+    oi.quantity * p.cost
+) as profit,
+--c.CITY,
+--c.STATE
+CASE
+        WHEN r.ORDER_ITEM_ID IS NULL THEN 'N'
+        ELSE 'Y'
+    END AS RETURN_FLAG
 FROM {{ ref('stg_orders_retail') }} o
-
 INNER JOIN {{ ref('stg_customers_retail') }} c
+    ON o.CUSTOMER_ID = c.CUSTOMER_ID
+INNER JOIN {{ ref('stg_order_items_retail') }} oi
+    ON o.ORDER_ID = oi.ORDER_ID
+INNER JOIN {{ ref('stg_products_retail') }} p
+    ON oi.PRODUCT_ID = p.PRODUCT_ID
+LEFT JOIN {{ ref('stg_returns_retail') }} r
+    ON oi.ORDER_ITEM_ID = r.ORDER_ITEM_ID
 
-ON o.CUSTOMER_ID = c.CUSTOMER_ID
+)
+
+SELECT *,
+       ROUND((PROFIT / NET_SALES) * 100, 2) AS PROFIT_MARGIN
+FROM sales_calc
+
+
